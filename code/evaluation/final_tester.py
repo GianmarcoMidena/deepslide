65a5f134cf708782d09ea5982b388b2b48865923
Gianmarco Midena <gianmarco.midena@gmail.com>
2020-04-22 09:45:34 +0200

    Adopt additional metrics for evaluation, e.g. recall, precision and F-score

--- code/evaluation/final_tester.py
+++ code/evaluation/final_tester.py
@@ -1,13 +1,16 @@
 import logging
 from glob import glob
 from pathlib import Path
-from typing import List, Tuple, Dict
+from typing import List, Tuple
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (confusion_matrix, f1_score, precision_score, recall_score, accuracy_score,
+                             balanced_accuracy_score)
 
 
 class FinalTester:
+    _UNKNOWN_CLASS = 'unknown'
+
     def __init__(self, classes: List[str]):
         """
         Args:
@@ -26,24 +29,19 @@ class FinalTester:
         """
         logging.info("Finding best confidence threshold...")
 
-        best_macro_avg_acc = 0
+        best_score = 0
         best_confidence_th = None
         for preds_i, confidence_th_i in self._search_predictions_and_confidence_th(inference_folder):
             true_labels_i = self._extract_true_labels(wsis_info)
             pred_labels_i = self._extract_pred_labels(preds_i, true_labels_i)
-            macro_avg_acc_i = self._calc_macro_avg_class_acc(true_labels=true_labels_i, pred_labels=pred_labels_i)
-            micro_avg_acc_i = self._calc_micro_avg_class_acc(true_labels=true_labels_i, pred_labels=pred_labels_i)
-            partial_macro_avg_acc_i = self._calc_macro_avg_class_acc(true_labels=true_labels_i,
-                                                                     pred_labels=pred_labels_i, dropna=True)
-            partial_micro_avg_acc_i = self._calc_micro_avg_class_acc(true_labels=true_labels_i,
-                                                                     pred_labels=pred_labels_i, dropna=True)
-            logging.info(f"predictions with confidence > {confidence_th_i} "
-                         f"has macro average class accuracy {macro_avg_acc_i:.5f} "
-                         f"({partial_macro_avg_acc_i:.5f} without unknowns), "
-                         f"micro average class accuracy {micro_avg_acc_i:.5f} "
-                         f"({partial_micro_avg_acc_i:.5f} without unknowns)")
-            if macro_avg_acc_i > best_macro_avg_acc:
-                best_macro_avg_acc = macro_avg_acc_i
+            metrics_i = self._calc_metrics(true_labels_i, pred_labels_i)
+            conf_matrix_i = self._conf_matrix(true_labels_i, pred_labels_i)
+            logging.info(f"predictions with confidence > {confidence_th_i}, "
+                         f"\n{metrics_i} "
+                         f"\n{conf_matrix_i}")
+            f_score_avg = metrics_i['f_score_avg']
+            if f_score_avg > best_score:
+                best_score = f_score_avg
                 best_confidence_th = confidence_th_i
         logging.info(f"best confidence threshold: {best_confidence_th}")
         return best_confidence_th
@@ -61,16 +59,10 @@ class FinalTester:
         for predictions_i in self._search_predictions(inference_folder):
             true_labels_i = self._extract_true_labels(wsis_info)
             pred_labels_i = self._extract_pred_labels(predictions_i, true_labels_i)
-            macro_avg_acc_i = self._calc_macro_avg_class_acc(true_labels=true_labels_i, pred_labels=pred_labels_i)
-            micro_avg_acc_i = self._calc_micro_avg_class_acc(true_labels=true_labels_i, pred_labels=pred_labels_i)
-            partial_macro_avg_acc_i = self._calc_macro_avg_class_acc(true_labels=true_labels_i,
-                                                                     pred_labels=pred_labels_i, dropna=True)
-            partial_micro_avg_acc_i = self._calc_micro_avg_class_acc(true_labels=true_labels_i,
-                                                                     pred_labels=pred_labels_i, dropna=True)
+            metrics_i = self._calc_metrics(true_labels_i, pred_labels_i)
             conf_matrix_i = self._conf_matrix(true_labels_i, pred_labels_i)
             logging.info(f"test set has final "
-                         f"macro avg acc: {macro_avg_acc_i:.5f} ({partial_macro_avg_acc_i:.5f} without unknowns), "
-                         f"micro avg acc: {micro_avg_acc_i:.5f} ({partial_micro_avg_acc_i:.5f} without unknowns)"
+                         f"\n{metrics_i} "
                          f"\n{conf_matrix_i}")
 
     @staticmethod
@@ -91,7 +83,7 @@ class FinalTester:
                             .sort_index()
 
     def _calc_micro_avg_class_acc(self, true_labels: pd.Series, pred_labels: pd.Series,
-                                  dropna: bool=False) -> Tuple[float, np.ndarray]:
+                                  dropna: bool = False) -> Tuple[float, np.ndarray]:
         """
         Find the micro average class accuracy of the predictions.
 
@@ -110,7 +102,7 @@ class FinalTester:
         return n_right_preds / n_examples
 
     def _calc_macro_avg_class_acc(self, true_labels: pd.Series, pred_labels: pd.Series,
-                                  dropna: bool=False) -> Tuple[float, np.ndarray]:
+                                  dropna: bool = False) -> Tuple[float, np.ndarray]:
         """
         Find the macro average class accuracy of the predictions.
 
@@ -132,8 +124,7 @@ class FinalTester:
         acc_by_class = n_right_preds_by_label.div(np.maximum(n_examples_by_true_label, 1))
         return acc_by_class.mean()
 
-    def _conf_matrix(self, true_labels: pd.Series,
-                     pred_labels: pd.Series) -> Tuple[float, np.ndarray]:
+    def _conf_matrix(self, true_labels: pd.Series, pred_labels: pd.Series) -> pd.Series:
         """
         Calculate the confusion matrix.
 
@@ -144,8 +135,12 @@ class FinalTester:
         Returns:
             A tuple containing the confusion matrix.
         """
-        conf_matrix = confusion_matrix(y_true=true_labels, y_pred=pred_labels.fillna('Unknown'))
-        return conf_matrix
+        conf_matrix = confusion_matrix(y_true=true_labels, y_pred=pred_labels.fillna(self._UNKNOWN_CLASS),
+                                       labels=self._classes + [self._UNKNOWN_CLASS])
+        return pd.DataFrame(conf_matrix[:len(self._classes)], index=self._classes,
+                            columns=self._classes + [self._UNKNOWN_CLASS])\
+                 .rename_axis(index='ground truth') \
+                 .rename_axis(columns='predictions')
 
     @staticmethod
     def _parse_confidence_th(csv_path: Path) -> float:
@@ -174,3 +169,34 @@ class FinalTester:
             predictions_i = pd.read_csv(prediction_csv_path_i)
             confidence_th_i = self._parse_confidence_th(csv_path=prediction_csv_path_i)
             yield predictions_i, confidence_th_i
+
+    def _calc_metrics(self, y_true, y_pred):
+        y_pred = y_pred.fillna(self._UNKNOWN_CLASS)
+        f_score_per_class = self._score_per_class(f1_score, y_true, y_pred)
+        f_score_micro = f1_score(y_true=y_true, y_pred=y_pred, labels=self._classes, average='micro')
+        f_score_macro = f1_score(y_true=y_true, y_pred=y_pred, labels=self._classes, average='macro')
+        f_score_avg = (f_score_micro + f_score_macro) / 2
+        precision_per_class = self._score_per_class(precision_score, y_true, y_pred)
+        precision_micro = precision_score(y_true=y_true, y_pred=y_pred, labels=self._classes, average='micro')
+        precision_macro = precision_score(y_true=y_true, y_pred=y_pred, labels=self._classes, average='macro')
+        precision_avg = (precision_micro + precision_macro) / 2
+        recall_per_class = self._score_per_class(recall_score, y_true, y_pred)
+        recall_micro = recall_score(y_true=y_true, y_pred=y_pred, labels=self._classes, average='micro')
+        recall_macro = recall_score(y_true=y_true, y_pred=y_pred, labels=self._classes, average='macro')
+        recall_avg = (recall_micro + recall_macro) / 2
+        accuracy_macro = balanced_accuracy_score(y_true=y_true, y_pred=y_pred)
+        accuracy_micro = accuracy_score(y_true=y_true, y_pred=y_pred)
+        accuracy_avg = (accuracy_micro + accuracy_macro) / 2
+        return pd.Series({
+            **{f"f_score_{label}": score for label, score in f_score_per_class.items()},
+            'f_score_macro': f_score_macro, 'f_score_micro': f_score_micro, 'f_score_avg': f_score_avg,
+            **{f"precision_{label}": score for label, score in precision_per_class.items()},
+            'precision_macro': precision_macro, 'precision_micro': precision_micro, 'precision_avg': precision_avg,
+            **{f"recall_{label}": score for label, score in recall_per_class.items()},
+            'recall_macro': recall_macro, 'recall_micro': recall_micro, 'recall_avg': recall_avg,
+            'accuracy_micro': accuracy_micro, 'accuracy_macro': accuracy_macro, 'accuracy_avg': accuracy_avg
+        })
+
+    def _score_per_class(self, score_fn, y_true, y_pred):
+        score = score_fn(y_true=y_true, y_pred=y_pred, labels=self._classes, average=None)
+        return {label_i: score_i for label_i, score_i in zip(self._classes, score)}
